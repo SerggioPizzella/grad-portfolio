ccff4766d7e06337a603355da6d245850f0531b5
Serggio Pizzella <serggio.pizzella@infosupport.com>
2024-11-08 12:20:27 +0100

    Quartz sync: Nov 8, 2024, 12:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "Serggio Pizzella",
+    pageTitleSuffix: "Graduation Portfolio",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog.serggio.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
